71dd0ef814db54bc6a0eabfdb9c9d64ea3e6c01c
Gareth Jones <java.jones@gmail.com>
2023-06-17 12:11:47 -0600

    Added fetch app

---
+++ FetchRepo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LibGit2Sharp;
+
+namespace FetchRepo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = Configuration.BuildFrom(null);
+            var httpsProxy = config.GetValueOrDefault<string>("https.proxy") ?? "not set";
+            var httpProxy = config.GetValueOrDefault<string>("http.proxy") ?? "not set";
+            Console.WriteLine($"Global git proxy settings, https.proxy: {httpsProxy} http.proxy: {httpProxy}");
+            var url = "https://github.com/Glysade/DataFxns";
+            var root = "C:/tmp/DataFxns";
+            Repository.Clone(url, root);
+        }
+    }
+}
